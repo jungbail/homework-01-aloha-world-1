493004340f529da79b0bee9b4c4c9e14c2867d53
jungbail <baileykjung@gmail.com>
2026-01-21 00:15:56 -0500

    added constructors for greeting taking different types of arguments

--- src/main/java/student/Greeting.java
+++ src/main/java/student/Greeting.java
@@ -8,5 +8,39 @@ package student;
  */
 public class Greeting {
 
+    private int locality;
+
+    private static String localityName;
+
+    private static String asciiGreeting;
+
+    private static String unicodeGreeting;
+
+    private static String formatting;
+
+
+
+    // for when Greeter only gets locality and  locality name arguments
+    public Greeting(int locality, String localityName){
+        this.locality = locality;
+        this.localityName = localityName;
+    }
+
+    //for when Greeting gets locality, locality name, and ascii greeting arguments
+    public Greeting(int locality, String localityName, String asciiGreeting){
+        this.locality = locality;
+        this.localityName = localityName;
+        this.asciiGreeting = asciiGreeting;
+    }
+
+    //for when Greeting gets locality, locality name, ascii greeting arguments, unicodeGreetings and formatting
+    public Greeting(int locality, String localityName, String asciiGreeting, String unicodeGreeting, String formating){
+        this.locality = locality;
+        this.localityName = localityName;
+        this.asciiGreeting = asciiGreeting;
+        this.unicodeGreeting = unicodeGreeting;
+        this.formatting = formatting;
+
+    }
 
 }
